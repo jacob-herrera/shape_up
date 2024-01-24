28dc2ef820fcac9a7c07e9fed0bb1d52f3422f29
Jacob Herrera <jacoblherrera@outlook.com>
2024-01-24 14:57:33 -0600

    audio defualts

--- scripts/volume_slider.gd
+++ scripts/volume_slider.gd
@@ -7,6 +7,7 @@ var bus_idx: int
 
 func _ready() -> void:
 	bus_idx = AudioServer.get_bus_index(bus_name)
+	AudioServer.set_bus_volume_db(bus_idx,linear_to_db(value))
 	value_changed.connect(_on_val_changed)
 
 func _on_val_changed(val: float) -> void:
