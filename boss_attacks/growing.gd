1981949bef263aed76a280c86b0b16482dbc3660
Jacob Herrera <jacoblherrera@outlook.com>
2024-01-31 22:42:16 -0600

    slow homing missle

--- boss_attacks/growing.gd
+++ boss_attacks/growing.gd
@@ -4,12 +4,20 @@ extends Node3D
 @export var mesh: MeshInstance3D
 
 @export var grow_rate: float
+@export var max_life_time: float
+@export var delay: float
 
+var life_time: float = 0.0
 var grow_vec: Vector3
 
 func _ready() -> void:
 	grow_vec = Vector3(grow_rate, grow_rate, grow_rate) 
 
 func _process(delta: float) -> void:
+	life_time += delta
+	if life_time <= delay:
+		return
 	col.scale += grow_vec * delta
 	mesh.scale += grow_vec * delta
+	if life_time >= max_life_time:
+		queue_free()
--- boss_attacks/homing_missle.gd
+++ boss_attacks/homing_missle.gd
@@ -1,9 +1,28 @@
 extends Node3D
 
 @export var speed: float
+@export var homing_power: float
 
 func _process(delta: float) -> void:
-	var target: Transform3D = Character.transform
-	var dir: Vector3 = global_position.direction_to(target.origin)
-	global_position += dir * speed * delta
-	look_at(target.origin)
+	#var original
+	var target: Vector3 = Character.global_position
+	var dir: Vector3 = global_position.direction_to(target)
+	var angle_to_player: float = global_position.angle_to(target)
+	#rotate(dir, angle_to_player)
+	var original_basis: Basis = basis
+	look_at(target)
+	var goal_q = Quaternion(basis)
+	var current_q = Quaternion(original_basis)
+	
+	var t: float = pow(0.5, delta * homing_power)
+	#clock_speed = lerpf(target_clock_speed, clock_speed, t)
+	var mid = goal_q.slerp(current_q, t)
+	transform.basis = Basis(mid)
+	
+	
+	
+	global_position += -basis.z * speed * delta
+	#rotation = move_toward(rotation, angle_to_player, delta)
+	#var dir: Vector3 = global_position.direction_to(target.origin)
+	#
+	#look_at(target.origin)
