8fc57e37a42313809f4baf9e2f301faf5e882f44
ardie01 <arman.heydarian01@gmail.com>
2024-02-04 22:01:43 -0600

    Added Submenus for Sounds, Graphics, and Controls
    yeah

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,9 +2,13 @@ extends Control
 
 var enabled: bool = false
 var in_settings: bool = false
+var in_submenu: bool = false
 
 @onready var main: VBoxContainer = $Margin/Main
 @onready var settings: VBoxContainer = $Margin/Settings
+@onready var sounds: VBoxContainer = $Margin/Sounds
+@onready var graphics: VBoxContainer = $Margin/Graphics
+@onready var controls: VBoxContainer = $Margin/Controls
 
 func _ready() -> void:
 	main.visible = true
@@ -12,6 +16,8 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	hide()
 	settings.hide()
+	sounds.hide()
+	graphics.hide()
 
 func reset() -> void:
 	%EffectsVolume._on_val_changed(%EffectsVolume.value)
@@ -20,6 +26,8 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("menu"):
 		if in_settings:
 			_toggle_settings()
+		elif in_submenu:
+			_toggle_submenu(settings)
 		else:
 			_toggle_menu()
 
@@ -42,9 +50,24 @@ func _toggle_settings() -> void:
 	if in_settings:
 		settings.hide()
 		main.show()
-	else :
+		sounds.hide()
+		graphics.hide()
+		controls.hide()
+	else:
 		settings.show()
 		main.hide()
+		sounds.hide()
+		graphics.hide()
+		controls.hide()
+	in_settings = not in_settings
+
+func _toggle_submenu(which_menu : VBoxContainer) -> void:
+	settings.hide()
+	sounds.hide()
+	graphics.hide()
+	controls.hide()
+	which_menu.show()
+	in_submenu = not in_submenu
 	in_settings = not in_settings
 
 func _fullscreen_toggle(full):
@@ -69,3 +92,11 @@ func _lobby_pressed() -> void:
 func _quit_pressed() -> void:
 	get_tree().quit(0)
 
+func _on_sounds_pressed():
+	_toggle_submenu(sounds)
+
+func _on_graphics_pressed():
+	_toggle_submenu(graphics)
+
+func _on_controls_pressed():
+	_toggle_submenu(controls)
